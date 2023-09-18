aeb6035835a8dd86a22e76ac4e377f5d2f58407f
Andris Reinman <andris@reinman.eu>
2023-09-18 14:20:55 +0300

    v1.3.1

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 const { generateSummary, DEFAULT_SYSTEM_PROMPT, DEFAULT_USER_PROMPT } = require('./lib/generate-summary');
 const riskAnalysis = require('./lib/risk-analysis');
+const generateEmbeddings = require('./lib/generate-embeddings');
 
 module.exports = {
     generateSummary,
+    generateEmbeddings,
     riskAnalysis,
     DEFAULT_SYSTEM_PROMPT,
     DEFAULT_USER_PROMPT
