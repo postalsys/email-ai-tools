5c40f1504f969cb39270edf28e06e2ea65681fc1
Andris Reinman <andris@reinman.eu>
2023-09-27 11:55:33 +0300

    feat(embeddings): Support for embeddings query

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 
 const { generateSummary, DEFAULT_SYSTEM_PROMPT, DEFAULT_USER_PROMPT } = require('./lib/generate-summary');
 const riskAnalysis = require('./lib/risk-analysis');
-const { generateEmbeddings } = require('./lib/generate-embeddings');
+const { generateEmbeddings, getChunkEmbeddings } = require('./lib/generate-embeddings');
+const { embeddingsQuery } = require('./lib/embeddings-query');
 
 module.exports = {
     generateSummary,
     generateEmbeddings,
+    getChunkEmbeddings,
+    embeddingsQuery,
     riskAnalysis,
     DEFAULT_SYSTEM_PROMPT,
     DEFAULT_USER_PROMPT
