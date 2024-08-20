64ec2e7148be9d83a255a08f9ab50a934eeacf99
Andris Reinman <andris@reinman.eu>
2024-08-20 12:11:45 +0300

    fix(deps): Updated undici

--- .ncurc.js
+++ .ncurc.js
@@ -2,9 +2,6 @@ module.exports = {
     upgrade: true,
     reject: [
         // Block package upgrades that moved to ESM
-        'nanoid',
-
-        // no support for Node 16
-        'undici'
+        'nanoid'
     ]
 };
