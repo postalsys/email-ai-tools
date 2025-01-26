1097be6a6241e2b9bfcece2e8bb07fe078889602
Andris Reinman <andris@reinman.eu>
2025-01-26 17:15:16 +0200

    fix(deps): Bumped deps

--- .ncurc.js
+++ .ncurc.js
@@ -2,6 +2,7 @@ module.exports = {
     upgrade: true,
     reject: [
         // Block package upgrades that moved to ESM
-        'nanoid'
+        'nanoid',
+        'eslint-config-prettier'
     ]
 };
